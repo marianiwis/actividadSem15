7042cf2d0f835ae26d777f0e6ab7b4b6fae81551
marianiwis <marichulaprof@gmail.com>
2024-10-29 18:03:33 -0300

    ponele

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const elemento = document.getElementById('miElemento');
-    if (elemento) {
-        elemento.addEventListener('click', () => {
-            console.log('Elemento clickeado');
-        });
-    } else {
-        console.error('Elemento no encontrado');
-    }
-});
+
